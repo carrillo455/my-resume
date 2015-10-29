60f7a5b76df5560a98ec9249c5a2d8660a91c4d0
carrillo455 <sistemas.carrillo01@gmail.com>
2015-10-29 16:48:30 -0600

    Changed style.display to style.backgroundColor in the script at index.html and added 'charEscape' function at resumeBuilder.js

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -86,3 +86,12 @@ var projects = {
 		}
 	]
 }
+
+var charEscape = function(_html) {
+	var newHTML = _html;
+
+	newHTML = _html.replace(/</g, "&lt;");
+	newHTML = newHTML.replace(/>/g, "&gt;");
+
+	return newHTML;
+};
